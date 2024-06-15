5c613659ad734ad3a94e5ef11352bb30a5d54736
Preet Karwal <preetverma365@gmail.com>
2024-06-15 07:10:54 +0530

    updated social links and icons

--- src/sections/footer/data.js
+++ src/sections/footer/data.js
@@ -1,6 +1,6 @@
 import {AiOutlineInstagram} from 'react-icons/ai'
 import {AiOutlineTwitter} from 'react-icons/ai'
-import {AiOutlineDribbble} from 'react-icons/ai'
+// import {AiOutlineDribbble} from 'react-icons/ai'
 import {AiFillGithub} from 'react-icons/ai'
 
 export const links = [
@@ -13,8 +13,8 @@ export const links = [
 
 
 export const socials = [
-    {id: 1, link: 'https://instagram.com', icon: <AiOutlineInstagram/>},
-    {id: 2, link: 'https://twitter.com', icon: <AiOutlineTwitter/>},
-    {id: 3, link: 'https://dribbble.com', icon: <AiOutlineDribbble/>},
-    {id: 4, link: 'https://github.com', icon: <AiFillGithub/>}
+    {id: 1, link: 'https://www.instagram.com/preet_karwal/', icon: <AiOutlineInstagram/>},
+    {id: 2, link: 'https://x.com/preet365', icon: <AiOutlineTwitter/>},
+    // {id: 3, link: 'https://dribbble.com', icon: <AiOutlineDribbble/>},
+    {id: 4, link: 'https://github.com/Mr-Elegant', icon: <AiFillGithub/>}
 ]
--- src/sections/header/data.js
+++ src/sections/header/data.js
@@ -1,13 +1,13 @@
 import {AiOutlineInstagram} from 'react-icons/ai'
-import {AiOutlineTwitter} from 'react-icons/ai'
-import {AiOutlineDribbble} from 'react-icons/ai'
+import { FaSquareXTwitter } from "react-icons/fa6";
+// import {AiOutlineDribbble} from 'react-icons/ai'
 import {AiFillGithub} from 'react-icons/ai'
 
 const data = [
-    {id: 1, link: 'https://instagram.com', icon: <AiOutlineInstagram/>},
-    {id: 2, link: 'https://twitter.com', icon: <AiOutlineTwitter/>},
-    {id: 3, link: 'https://dribbble.com', icon: <AiOutlineDribbble/>},
-    {id: 4, link: 'https://github.com', icon: <AiFillGithub/>}
+    {id: 1, link: 'https://www.instagram.com/preet_karwal/', icon: <AiOutlineInstagram/>},
+    {id: 2, link: 'https://x.com/preet365', icon: <FaSquareXTwitter/>},
+    // {id: 3, link: 'https://dribbble.com', icon: <AiOutlineDribbble/>},
+    {id: 4, link: 'https://github.com/Mr-Elegant', icon: <AiFillGithub/>}
 ]
 
 export default data
