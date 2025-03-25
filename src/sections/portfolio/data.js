9bb14571e31d85992c3e4742820187927c6ee69f
Preet Karwal <preetverma365@gmail.com>
2025-03-25 12:28:16 +0530

    updated projects

--- src/sections/portfolio/data.js
+++ src/sections/portfolio/data.js
@@ -3,18 +3,37 @@ import ChatAPP from '../../assets/Chatapp.jpg'
 import Preetflix from '../../assets/preetflix.jpg'
 import YtClone from '../../assets/ytclone.jpg'
 import Ecom1 from '../../assets/ecommerce.jpg'
+import MG21 from '../../assets/mg2.1.jpg'
+import F4E from '../../assets/F4E.jpg'
 
 
 
 const data = [
     
     {
-        id: 1,  
+        id: 1,
+        
+        image: MG21,
+        title: "Movie Guider 2",
+        desc: "A Fully Resoponsive mega website for movie details ",
+        demo: "https://movieguider2.vercel.app/",
+        github: 'https://github.com/Mr-Elegant/MovieGuider2res'
+    },
+    {
+        id: 2,  
         image: MovieGuider ,
         title: "Movie Guider",
         desc: "A light-weight clone functionality of Imdb and using tmdb api . ",
         demo: 'https://movieguider.netlify.app',
         github: 'https://github.com/Mr-Elegant/Movie_Guider'
+    },
+    {
+        id: 3,  
+        image:  F4E,
+        title: "Friends4Ever Realtime Chat App",
+        desc: "A Realtime chat Application using Mern Stack V2 . ",
+        demo: 'https://friends4ever-chat-app2.onrender.com/',
+        github: 'https://github.com/Mr-Elegant/Friends4Ever-Chat-app2-Mern-'
     },
     {
         id: 4,
@@ -25,15 +44,7 @@ const data = [
         github: 'https://github.com/Mr-Elegant/chat-app-transparent'
     },
     {
-        id: 2,
-        
-        image: Preetflix,
-        title: "Netflix clone",
-        desc: "A NETFLIX using core php and mysql. you can stream any video like netflix",
-        github: 'https://github.com/Mr-Elegant/preetflix'
-    },
-    {
-        id: 3,
+        id: 5,
         
         image: Ecom1,
         title: "MERN Ecommerce app",
@@ -41,8 +52,18 @@ const data = [
         demo: "https://mern-ecommerce-app1.onrender.com/",
         github: 'https://github.com/burakorkmez/mern-ecommerce'
     },
+    
     {
-        id: 5,
+        id: 6,
+        
+        image: Preetflix,
+        title: "Netflix clone",
+        desc: "A NETFLIX using core php and mysql. you can stream any video like netflix",
+        github: 'https://github.com/Mr-Elegant/preetflix'
+    },
+    
+    {
+        id: 7,
         image: YtClone,
         title: "Youtube clone",
         desc: "A Youtube with dark mode clone by using reactjs . ",
